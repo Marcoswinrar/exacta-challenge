d472155087e745b96d5bdf9b05509fd224642685
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-06 01:20:39 -0300

    feat: Páginas de Home e Usuário

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+import * as S from './styled'
+
+const Home = () => (
+  <Layout>
+    <div>Cadastro</div>
+  </Layout>
+)
+
+export default Home
---
+++ src/pages/Users/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+import * as S from './styled'
+
+const Users = () => (
+  <Layout>
+    <div>Delicia</div>
+  </Layout>
+)
+
+export default Users
