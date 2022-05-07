07f83a3504d9385d1b94ce66467cacac85ab13f1
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-07 03:05:15 -0300

    update: Ajustes no form para compor dados de input

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,28 +1,64 @@
+import { useEffect, useState } from 'react'
 import Button from '../Button'
 import Input from '../Input'
+import Select from '../Select'
 import UserButtonGender from '../UserButtonGender'
+import { orgaoEmissorData } from '../../utils/mock'
 import * as S from './styled'
 
 const UserForm = () => {
+
+  const [data, setData] = useState({
+    nome: '',
+    rg: '',
+    dataEmissao: '',
+    orgaoEmissor: '',
+    sexo: ''
+  })
+
+  const changeFormData = (value, prop) =>
+    setData(prev => ({ ...prev, [prop]: value }))
+
   return (
     <S.UserFormWrapper>
       <S.UserFormInputContainer>
-        <Input type={"text"} label={"Nome"} hasSeparator />
-        <Input type={"text"} label={"Número do RG"} />
+        <Input
+          type={"text"}
+          label={"Nome"}
+          hasSeparator
+          onChange={(value) => changeFormData(value, 'nome')}
+        />
+        <Input
+          type={"text"}
+          label={"Número do RG"}
+          mask={"99 999 999 - 9"}
+          onChange={(value) => changeFormData(value, 'rg')}
+        />
       </S.UserFormInputContainer>
       <S.UserFormInputContainer>
-        <Input type={"text"} label={"Data de Emissão"} hasSeparator />
-        <Input type={"text"} label={"Orgão Expedidor"} />
+        <Input
+          type={"text"}
+          label={"Data de Emissão"}
+          mask={"99/99/9999"}
+          hasSeparator
+          onChange={(value) => changeFormData(value, 'dataEmissao')}
+        />
+        <Select
+          label={"Orgão Emissor"}
+          data={orgaoEmissorData}
+          onChange={(value) => changeFormData(value, 'orgaoEmissor')}
+        />
       </S.UserFormInputContainer>
       <S.UserFormInputContainer>
-        <UserButtonGender />
+        <UserButtonGender
+          handleClick={(value) => changeFormData(value, 'sexo')}
+        />
       </S.UserFormInputContainer>
       <S.UserFormInputContainer>
         <Button width={"250px"} value={"Cadastrar"} />
       </S.UserFormInputContainer>
     </S.UserFormWrapper>
   )
-
 }
 
 export default UserForm
