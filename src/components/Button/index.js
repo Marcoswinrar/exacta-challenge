040e653819c26a4da1327f7f302755dc16f6ccf1
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-09 00:53:32 -0300

    update: Ajuste em componente de button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,10 @@
 import * as S from './styled'
 
-const Button = ({ width, value, onClick, focusColor }) => {
+const Button = ({ width, value, onClick, active, ...props }) => {
   return (
     <S.ButtonWrapper
-      focusColor={focusColor}
+      {...props}
+      style={{ backgroundColor: active && '#22a6b3'}}
       width={width}
       onClick={onClick}>
       {value}
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -11,15 +11,10 @@ export const ButtonWrapper = styled.button`
   padding: 5px;
   cursor: pointer;
   box-shadow: 0 1px #999;
-
+  
   :active {
     box-shadow: 0 3px #666;
     transform: translateY(4px);
-  }
-
-  :focus {
-    background-color: ${props => props.focusColor && '#22a6b3'};
-    transition: all .3s ease-in-out;
   }
 
   ${media.lessThan("320px")`
