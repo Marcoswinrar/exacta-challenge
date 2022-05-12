c35ffd0a86ac2058ddd85dad1300d0a5594f7247
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-11 23:47:17 -0300

    refac: Exportando estilos de Input

---
+++ src/components/Input/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { UserFormFieldStyle } from '../../config/styles'
+
+export const Input = styled.input`
+  ${UserFormFieldStyle} 
+
+`
+export const InputLabel = styled.label`
+  font-size: .8rem;
+  text-transform: uppercase;
+  font-weight: bolder;
+`
