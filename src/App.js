9279e607bac9a0227bde2b83aee257b9af7fd570
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-11 23:45:20 -0300

    Update: contexto de usuário e arquivos deletados

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Users from './pages/Users'
+import AppRoutes from "./config/routes"
+import { UsersProvider } from "./contexts/userContext"
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        {/* <Route path="*" element={<Page404 />} /> */}
-      </Routes>
+      <UsersProvider>
+        <AppRoutes />
+      </UsersProvider>
     </div>
   )
 }
