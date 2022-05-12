c7e4fd5689c0ffa9c06c932b20af1e26f7ca9f0d
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-11 23:46:17 -0300

    feat: Adição de modal de delete

---
+++ src/components/DeleteModal/index.js
@@ -0,0 +1,49 @@
+import { userContext } from "../../contexts/userContext"
+import Button from "../Button"
+import Modal from "../Modal"
+import Fetch from '../../utils/fetch'
+import * as S from './styled'
+
+const DeleteModal = ({ user, onClose }) => {
+  const { removeUser, notify } = userContext()
+
+  const handleRemove = async () => {
+    await Fetch.delete(`users/${user.id}`)
+      .then(() => {
+        removeUser(user.id)
+        notify("Usuário excluído com sucesso!", "success")
+      })
+      .catch(error => {
+        if (error.response) {
+          notify("Houve um problema ao excluír", "error")
+        }
+      })
+      .finally(() => onClose())
+  }
+
+  return (
+    <Modal
+      title={"Excluir Usuário"}
+      onClose={onClose}
+    >
+      <S.Content>
+        <S.Text>Tem certeza que deseja excluir esse registro?</S.Text>
+        <S.ButtonContainer>
+          <Button
+            type="submit"
+            value="Excluir"
+            onClick={() => handleRemove()}
+          />
+          <Button
+            type="button"
+            value="Cancelar"
+            onClick={onClose}
+          />
+        </S.ButtonContainer>
+      </S.Content>
+    </Modal>
+  )
+}
+
+export default DeleteModal
+
---
+++ src/components/DeleteModal/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Content = styled.div``
+export const Text = styled.p`
+  color: black;
+  text-align: center;
+  padding: 15px;
+  font-size: 1.3rem;
+`
+export const ButtonContainer = styled.div`
+  display:flex;
+  justify-content: center;
+
+  button {
+    margin: 5px;
+  }
+`
